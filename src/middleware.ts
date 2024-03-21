15427c2d0e8f4fcd03ed3a3d43732454e3064f77
Ali H <alisonherrera02@gmail.com>
2024-03-20 22:26:26 -0400

    brought over middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,14 +1,20 @@
-import { authMiddleware, withClerkMiddleware } from '@clerk/nextjs';
-import { NextResponse } from 'next/server';
+import { authMiddleware } from '@clerk/nextjs';
 
-// See https://clerk.com/docs/references/nextjs/auth-middleware
-// for more information about configuring your Middleware
+// Routes that can be accessed while signed in
+// const signedInRoutes = ["/chat"];
 
-export default withClerkMiddleware(() => {
-  console.log('middleware running');
-  return NextResponse.next();
+export default authMiddleware({
+  // Routes that can be accessed while signed out
+  publicRoutes: ['/sign-in', '/', '/sign-up'],
+
+  // Routes that can always be accessed, and have
+  // no authentication information
+  ignoredRoutes: ['/no-auth-in-this-route', '/trpc/ai.generateText'],
 });
 
 export const config = {
-  matcher: ['/((?!.+\\.[\\w]+$|_next).*)'],
+  // Protects all routes, including api/trpc.
+  // See https://clerk.com/docs/references/nextjs/auth-middleware
+  // for more information about configuring your Middleware
+  matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
 };
