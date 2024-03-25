cfe24084ea81c330186989b0af851cea44b4eefe
Victoria <vmcnorrill@gmail.com>
2024-03-25 14:58:57 -0400

    uncommented user btn

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,11 @@
-// import { SignedIn, UserButton } from '@clerk/nextjs';
-import { Box, Header as MantineHeader, Flex } from '@mantine/core';
-import { useMediaQuery } from '@mantine/hooks';
-import { theme } from '~/config/theme';
-import PinkLogo from '../../public/logo/logo-pink-dark.png';
-import Image from 'next/image';
-import { ReactNode } from 'react';
-import MobileSidebar from './Sidebar/mobileSidebar';
 import { SignedIn, UserButton } from '@clerk/nextjs';
+import { Box, Header as MantineHeader, Flex } from "@mantine/core";
+import { useMediaQuery } from "@mantine/hooks";
+import { theme } from "~/config/theme";
+import PinkLogo from "../../public/logo/logo-pink-dark.png";
+import Image from "next/image";
+import { ReactNode } from "react";
+import MobileSidebar from "./Sidebar/mobileSidebar";
 
 type HeaderProps = {
   children?: ReactNode;
@@ -17,20 +16,20 @@ export const Header: React.FC<HeaderProps> = ({ children }) => {
   const { colors, white } = theme;
   //
 
-  return (
-    <MantineHeader height={80} ml={isMobile ? 'px' : '250px'}>
-      <Flex m='md' direction='row' align='center' justify='space-between'>
-        <Image
-          src={PinkLogo}
-          alt='Your Health Ally Logo'
-          width={96}
-          height={54}
-        />
-        {isMobile ? <MobileSidebar /> : null}
-        <SignedIn>
-          <UserButton />
-        </SignedIn>
-      </Flex>
-    </MantineHeader>
-  );
+	return (
+		<MantineHeader height={80} ml={isMobile ? "px" : "250px"}>
+			<Flex m="md" direction="row" align="center" justify="space-between">
+				<Image
+					src={PinkLogo}
+					alt="Your Health Ally Logo"
+					width={96}
+					height={54}
+				/>
+				{isMobile ? <MobileSidebar /> : null}
+				<SignedIn>
+					<UserButton />
+				</SignedIn>
+			</Flex>
+		</MantineHeader>
+	);
 };
