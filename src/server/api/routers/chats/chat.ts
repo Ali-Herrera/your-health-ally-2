35ca4cfbf9832c48d726db3576a461992f02aed8
Ali H <alisonherrera02@gmail.com>
2024-04-15 16:10:08 -0400

    buidling chatRouter, adding user data to chats

--- src/server/api/routers/chats/chat.ts
+++ src/server/api/routers/chats/chat.ts
@@ -1,15 +1,58 @@
 import { z } from 'zod';
 // import { prisma } from '~/server/db';
 import { createTRPCRouter, publicProcedure } from '~/server/api/trpc';
-import { Session } from '@clerk/nextjs/server';
+import { User, clerkClient } from '@clerk/nextjs/server';
 import { PrismaClient } from '@prisma/client';
+import type { Chat } from '@prisma/client';
+
+const filterUserForClient = (user: User) => {
+  return {
+    id: user.id,
+    username: user.username,
+  };
+};
+
+const addUserDataToChats = async (chats: Chat[]) => {
+  const userId = chats.map((chat) => chat.userId);
+  const users = (
+    await clerkClient.users.getUserList({
+      userId: userId,
+      limit: 10,
+    })
+  ).map(filterUserForClient);
+
+  return chats.map((chat) => {
+    const user = users.find((user) => user.id === chat.userId);
+
+    return {
+      ...chat,
+      user,
+    };
+  });
+};
 
 export const chatRouter = createTRPCRouter({
-  getAll: publicProcedure.query(({ ctx }) => {
-    return ctx.prisma.chat.findMany({
-      orderBy: { createdAt: 'desc' },
+  getById: publicProcedure
+    .input(z.object({ id: z.string() }))
+    .query(async ({ ctx, input }) => {
+      const chat = await ctx.prisma.chat.findUnique({
+        where: { id: input.id },
+      });
+
+      if (!chat) {
+        throw new Error('Chat not found');
+      }
+
+      return (await addUserDataToChats([chat]))[0];
+    }),
+
+  getAll: publicProcedure.query(async ({ ctx }) => {
+    const chats = await ctx.prisma.chat.findMany({
       take: 10,
+      orderBy: [{ createdAt: 'desc' }],
     });
+
+    return addUserDataToChats(chats);
   }),
 });
 
