eec18de95bc8d0c65636442fc51d0fc093677fc1
Victoria <vmcnorrill@gmail.com>
2024-04-26 11:49:02 -0400

    removed any tooltip from chat content, fixed syntax errors

--- src/components/Chat/ChatContent.tsx
+++ src/components/Chat/ChatContent.tsx
@@ -1,11 +1,4 @@
-import {
-	Box,
-	Stack,
-	Group,
-	Avatar,
-	Skeleton,
-	Text,
-} from "@mantine/core";
+import { Box, Stack, Group, Avatar, Skeleton, Text } from "@mantine/core";
 import { useMediaQuery } from "@mantine/hooks";
 import { type Author } from "~/utils/types";
 import { UserButton } from "@clerk/nextjs";
@@ -53,36 +46,33 @@ export const ChatContent = ({ chatItems, loading }: Props) => {
 		scrollToBottom();
 	}, [chatItems]);
 
-
-
 	return (
+		<Box
+			ml={mobileScreen ? "0" : "250px"}
+			h="65vh"
+			sx={{ overflowY: "scroll", maxWidth: "100%" }}
+		>
+			{" "}
+			<Stack spacing="md">
+				{/* Render initial prompt only if no user messages exist */}
+				{!userMessageExists && (
+					<Group p="xl" sx={{ backgroundColor: "#E5E5E5" }}>
+						<Avatar size={32} alt="ChatGBT" variant="gradient" mb="sm">
+							AI
+						</Avatar>
 
-			<Box
-				ml={mobileScreen ? "0" : "250px"}
-				h="65vh"
-				sx={{ overflowY: "scroll", maxWidth: "100%" }}
-			>
-				{" "}
-				<Stack spacing="md">
-					{/* Render initial prompt only if no user messages exist */}
-					{!userMessageExists && (
-						<Group p="xl" sx={{ backgroundColor: "#E5E5E5" }}>
-							<Avatar size={32} alt="ChatGBT" variant="gradient" mb="sm">
-								AI
-							</Avatar>
-
-							<Text c={black}>
-								I am an intelligent advisor that can provide information
-								regarding people's health. I answer questions about
-								health-related conditions and symptoms, and how to prepare for
-								doctors appointments. This is for educational purposes only.
-								Please see your healthcare provider for medical treatment.
-							</Text>
-						</Group>
-					)}
-
-					{/* Render chat items */}
-					{chatItems.map((chatItem: ChatItem, index: number) => (
+						<Text c={black}>
+							I am an intelligent advisor that can provide information regarding
+							people's health. I answer questions about health-related
+							conditions and symptoms, and how to prepare for doctors
+							appointments. This is for educational purposes only. Please see
+							your healthcare provider for medical treatment.
+						</Text>
+					</Group>
+				)}
+				{/* Render chat items */}
+				{chatItems.map((chatItem: ChatItem, index: number) => {
+					return (
 						<Box key={index} data-id={index}>
 							{chatItem.author === "User" ? (
 								<Group
@@ -91,31 +81,18 @@ export const ChatContent = ({ chatItems, loading }: Props) => {
 									data-id={index}
 								>
 									<UserButton />
-									{needDictionary && (
-										<Tooltip
-											label={textDefinition}
-											color={black}
-											position="top"
-											closeDelay={900}
-											multiline
-											withArrow
-											opened={needDictionary}
-											events={{ hover: false, focus: true, touch: true }}
+									{chatItem.content?.includes("\n") ? (
+										<Text
+											component="pre"
+											sx={{
+												wordWrap: "break-word",
+												whiteSpace: "pre-wrap",
+											}}
 										>
-											{chatItem.content?.includes("\n") ? (
-												<Text
-													component="pre"
-													sx={{
-														wordWrap: "break-word",
-														whiteSpace: "pre-wrap",
-													}}
-												>
-													{chatItem.content}
-												</Text>
-											) : (
-												<Text>{chatItem.content}</Text>
-											)}
-										</Tooltip>
+											{chatItem.content}
+										</Text>
+									) : (
+										<Text>{chatItem.content}</Text>
 									)}
 								</Group>
 							) : (
@@ -123,56 +100,34 @@ export const ChatContent = ({ chatItems, loading }: Props) => {
 									<Avatar size={32} alt="ChatGBT" variant="gradient" mb="sm">
 										AI
 									</Avatar>
-
 									{chatItem.content?.includes("\n") ? (
-										<Tooltip
-											label={textDefinition}
-											color={black}
-											position="top"
-											closeDelay={900}
-											multiline
-											withArrow
-											opened={needDictionary}
-											events={{ hover: false, focus: true, touch: true }}
+										<Text
+											component="pre"
+											sx={{ wordWrap: "break-word", whiteSpace: "pre-wrap" }}
 										>
-											<Text
-												component="pre"
-												sx={{ wordWrap: "break-word", whiteSpace: "pre-wrap" }}
-											>
-												{chatItem.content}
-											</Text>
-										</Tooltip>
+											{chatItem.content}
+										</Text>
 									) : (
-										<Tooltip
-											label={textDefinition}
-											color={black}
-											position="top"
-											closeDelay={900}
-											multiline
-											withArrow
-											opened={needDictionary}
-											events={{ hover: false, focus: true, touch: true }}
-										>
-											<Text>{chatItem.content}</Text>
-										</Tooltip>
+										<Text>{chatItem.content}</Text>
 									)}
 								</Group>
 							)}
 						</Box>
-					))}
+					);
+				})}
 
-					{/* Display skeleton loading animation below loaded chat end if loading */}
-					{loading && (
-						<Group p="xl">
-							<Skeleton height={32} circle />
-							<Skeleton height={8} radius="xl" />
-							<Skeleton height={8} radius="xl" width="70%" />
-						</Group>
-					)}
+				{/* Display skeleton loading animation below loaded chat end if loading */}
+				{loading && (
+					<Group p="xl">
+						<Skeleton height={32} circle />
+						<Skeleton height={8} radius="xl" />
+						<Skeleton height={8} radius="xl" width="70%" />
+					</Group>
+				)}
 
-					{/* Empty div to scroll to when necessary */}
-					<div ref={endOfChatRef} />
-				</Stack>
-			</Box>
+				{/* Empty div to scroll to when necessary */}
+				<div ref={endOfChatRef} />
+			</Stack>
+		</Box>
 	);
 };
