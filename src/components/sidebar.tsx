6c74dfe8d9c03e558111bbdac7229b38ebf0ea43
Victoria <vmcnorrill@gmail.com>
2024-05-06 14:03:10 -0400

    started chat history design

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, Flex, Space } from "@mantine/core";
+import { Box, Button, Flex, Space, Stack, Text } from "@mantine/core";
 import { theme } from "../config/theme";
 
 type Props = {
@@ -6,7 +6,7 @@ type Props = {
 };
 
 export const Sidebar = ({ onReset }: Props) => {
-	const { colors } = theme;
+	const { colors, white } = theme;
 
 	return (
 		<Flex
@@ -30,6 +30,34 @@ export const Sidebar = ({ onReset }: Props) => {
 			<Box h={100} sx={{ borderBottom: colors?.darkPink?.[9] }}>
 				<Button onClick={onReset}>Start New Chat</Button>
 			</Box>
+
+			<Stack
+				h={300}
+				// bg="var(--mantine-color-body)"
+				align="stretch"
+				justify="center"
+				// gap="lg"
+			>
+				<Text c={white}>
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit...
+				</Text>
+				<Button.Group>
+					<Button
+						variant="white"
+						size="compact-md"
+						sx={{ marginBottom: "10px", border: "1px solid #4C1732" }}
+					>
+						Open
+					</Button>
+					<Button
+						variant="white"
+						size="compact-md"
+						sx={{ marginBottom: "10px", border: "1px solid #4C1732" }}
+					>
+						Delete
+					</Button>
+				</Button.Group>
+			</Stack>
 		</Flex>
 	);
 };
