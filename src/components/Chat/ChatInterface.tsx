be40f9d32784617c6076378844d7fdbfeb3972c2
Victoria <vmcnorrill@gmail.com>
2024-06-10 13:26:56 -0400

    implemented jsPDF to download chats

--- src/components/Chat/ChatInterface.tsx
+++ src/components/Chat/ChatInterface.tsx
@@ -1,118 +1,133 @@
-import { Box } from '@mantine/core';
-import { useMediaQuery } from '@mantine/hooks';
-import { ChatContent, type ChatItem } from '~/components/Chat/ChatContent';
-import { ChatInput } from '~/components/Chat/ChatInput';
-import { api } from '~/utils/api';
-import { useEffect, useState } from 'react';
-import { useUser } from '@clerk/nextjs';
-import { Sidebar } from '../sidebar';
-import { HeaderMobile } from '../header/mobileHeader';
-import { Header } from '../header';
-import { Author } from '~/utils/types';
-import { useQueryClient } from '@tanstack/react-query';
+import { Box } from "@mantine/core";
+import { useMediaQuery } from "@mantine/hooks";
+import { ChatContent, type ChatItem } from "~/components/Chat/ChatContent";
+import { ChatInput } from "~/components/Chat/ChatInput";
+import { api } from "~/utils/api";
+import { useEffect, useState } from "react";
+import { useUser } from "@clerk/nextjs";
+import { Sidebar } from "../sidebar";
+import { HeaderMobile } from "../header/mobileHeader";
+import { Header } from "../header";
+import { Author } from "~/utils/types";
+import { useQueryClient } from "@tanstack/react-query";
+const { jsPDF } = require("jspdf"); // will automatically load the node version
 
 interface ChatInterfaceProps {
-  userId: string;
+	userId: string;
 }
 
 const ChatInterface: React.FC<ChatInterfaceProps> = ({ userId }) => {
-  const mobileScreen = useMediaQuery('(max-width: 480px)');
-  const [chatItems, setChatItems] = useState<ChatItem[]>([]);
-  const [waiting, setWaiting] = useState<boolean>(false);
-  const { isLoaded, user } = useUser();
-  const [currentChat, setCurrentChat] = useState<string | null>(null);
-  const queryClient = useQueryClient();
+	const mobileScreen = useMediaQuery("(max-width: 480px)");
+	const [chatItems, setChatItems] = useState<ChatItem[]>([]);
+	const [waiting, setWaiting] = useState<boolean>(false);
+	const { isLoaded, user } = useUser();
+	const [currentChat, setCurrentChat] = useState<string | null>(null);
+	const queryClient = useQueryClient();
 
-  // TRPC mutations
-  const startNewChatMutation = api.chat.startNewChat.useMutation();
-  const deleteChatMutation = api.delete.deleteChat.useMutation();
+	// TRPC mutations
+	const startNewChatMutation = api.chat.startNewChat.useMutation();
+	const deleteChatMutation = api.delete.deleteChat.useMutation();
 
-  const handleUpdate = (prompt: string, chatId: string, author: Author) => {
-    setWaiting(true);
-    setChatItems((prevChatItems) => [
-      ...prevChatItems,
-      { content: prompt, author: author },
-    ]);
-    setWaiting(false);
-  };
+	const handleUpdate = (prompt: string, chatId: string, author: Author) => {
+		setWaiting(true);
+		setChatItems((prevChatItems) => [
+			...prevChatItems,
+			{ content: prompt, author: author },
+		]);
+		setWaiting(false);
+	};
 
-  const handleStartNewChat = async () => {
-    try {
-      const newChat = await startNewChatMutation.mutateAsync();
-      const newChatId = newChat?.chatId ?? '';
-      setCurrentChat(newChatId);
-      setChatItems([]);
-    } catch (error) {
-      console.error('Failed to start new chat:', error);
-    }
-  };
+	const handleStartNewChat = async () => {
+		try {
+			const newChat = await startNewChatMutation.mutateAsync();
+			const newChatId = newChat?.chatId ?? "";
+			setCurrentChat(newChatId);
+			setChatItems([]);
+		} catch (error) {
+			console.error("Failed to start new chat:", error);
+		}
+	};
 
-  const handleDeleteChat = async (chatId: string) => {
-    try {
-      await deleteChatMutation.mutateAsync({ chatId });
-      // Invalidate the query to refetch the chat list
-      queryClient.invalidateQueries(['api.chat.getAll']);
-    } catch (error) {
-      console.error('Error deleting chat:', error);
-      // Handle error
-    }
-  };
+	const handleDeleteChat = async (chatId: string) => {
+		try {
+			await deleteChatMutation.mutateAsync({ chatId });
+			// Invalidate the query to refetch the chat list
+			queryClient.invalidateQueries(["api.chat.getAll"]);
+		} catch (error) {
+			console.error("Error deleting chat:", error);
+			// Handle error
+		}
+	};
 
-  const handleRevisitChat = (chatId: string) => {
-    setCurrentChat(chatId);
-    // You can fetch the chat title here if needed
-  };
+	const handleRevisitChat = (chatId: string) => {
+		setCurrentChat(chatId);
+		// You can fetch the chat title here if needed
+	};
 
-  const getMessagesByChatId = api.chat.getMessagesByChatId.useQuery(
-    { chatId: currentChat || '' },
-    { enabled: !!currentChat }
-  );
+	const handleGeneratePDF = (chatId: string) => {
+		// setCurrentChat(chatId);
 
-  useEffect(() => {
-    if (getMessagesByChatId.data) {
-      const messages: ChatItem[] = getMessagesByChatId.data.map(
-        (message: any) => ({
-          author: message.userId === userId ? 'User' : 'AI',
-          content: message.content,
-        })
-      );
-      setChatItems([...messages]);
-    }
-  }, [getMessagesByChatId.data]);
+		// Generate PDF
+		const doc = new jsPDF();
+		doc.text(`Chat Transcript: ${chatId}`, 10, 10);
+		chatItems.forEach((item, index) => {
+			doc.text(`${item.author}: ${item.content}`, 10, 20 + index * 10);
+		});
+		doc.save(`chat-transcript-${chatId}.pdf`);
+	};
 
-  return (
-    <Box>
-      {isLoaded && user && (
-        <>
-          {mobileScreen ? (
-            <HeaderMobile
-              onStartNewChat={handleStartNewChat}
-              onRevisitChat={handleRevisitChat}
-              onDeleteChat={handleDeleteChat}
-            />
-          ) : (
-            <Header />
-          )}
-          {!mobileScreen && (
-            <Sidebar
-              onStartNewChat={handleStartNewChat}
-              onDeleteChat={handleDeleteChat}
-              onRevisitChat={handleRevisitChat}
-              chats={undefined}
-            />
-          )}
-          <ChatContent chatItems={chatItems} loading={waiting} />
-          <ChatInput
-            onUpdate={handleUpdate}
-            waiting={waiting}
-            userId={user.id}
-            currentChat={currentChat}
-            onStartNewChat={handleStartNewChat}
-          />
-        </>
-      )}
-    </Box>
-  );
+	const getMessagesByChatId = api.chat.getMessagesByChatId.useQuery(
+		{ chatId: currentChat || "" },
+		{ enabled: !!currentChat }
+	);
+
+	useEffect(() => {
+		if (getMessagesByChatId.data) {
+			const messages: ChatItem[] = getMessagesByChatId.data.map(
+				(message: any) => ({
+					author: message.userId === userId ? "User" : "AI",
+					content: message.content,
+				})
+			);
+			setChatItems([...messages]);
+		}
+	}, [getMessagesByChatId.data]);
+
+	return (
+		<Box>
+			{isLoaded && user && (
+				<>
+					{mobileScreen ? (
+						<HeaderMobile
+							onStartNewChat={handleStartNewChat}
+							onRevisitChat={handleRevisitChat}
+							onDeleteChat={handleDeleteChat}
+							onGeneratePDF={handleGeneratePDF}
+						/>
+					) : (
+						<Header />
+					)}
+					{!mobileScreen && (
+						<Sidebar
+							onStartNewChat={handleStartNewChat}
+							onDeleteChat={handleDeleteChat}
+							onRevisitChat={handleRevisitChat}
+							onGeneratePDF={handleGeneratePDF}
+							chats={undefined}
+						/>
+					)}
+					<ChatContent chatItems={chatItems} loading={waiting} />
+					<ChatInput
+						onUpdate={handleUpdate}
+						waiting={waiting}
+						userId={user.id}
+						currentChat={currentChat}
+						onStartNewChat={handleStartNewChat}
+					/>
+				</>
+			)}
+		</Box>
+	);
 };
 
 export default ChatInterface;
--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -1,217 +1,224 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 import {
-  Box,
-  Button,
-  Divider,
-  Flex,
-  Loader,
-  Menu,
-  ScrollArea,
-  Text,
-  Title,
-} from '@mantine/core';
-import { theme } from '../config/theme';
-import { api } from '~/utils/api';
+	Box,
+	Button,
+	Divider,
+	Flex,
+	Loader,
+	Menu,
+	ScrollArea,
+	Text,
+	Title,
+} from "@mantine/core";
+import { theme } from "../config/theme";
+import { api } from "~/utils/api";
 import {
-  IconDotsVertical,
-  IconFileDownload,
-  IconMessage,
-  IconTrash,
-} from '@tabler/icons-react';
+	IconDotsVertical,
+	IconFileDownload,
+	IconMessage,
+	IconTrash,
+} from "@tabler/icons-react";
 
 type Props = {
-  onStartNewChat: () => void;
-  onDeleteChat: (chatId: string) => void;
-  chats: any;
-  onRevisitChat: (chatId: string) => void;
+	onStartNewChat: () => void;
+	onDeleteChat: (chatId: string) => void;
+	chats: any;
+	onRevisitChat: (chatId: string) => void;
+	onGeneratePDF: (chatId: string) => void;
 };
 
 export const Sidebar = ({
-  onStartNewChat,
-  onDeleteChat,
-  onRevisitChat,
+	onStartNewChat,
+	onDeleteChat,
+	onRevisitChat,
+	onGeneratePDF,
 }: Props) => {
-  const { colors, white, black } = theme;
-  const [noPreviousChats, setNoPreviousChats] = useState(false);
-  const [errorMessage, setErrorMessage] = useState('');
+	const { colors, white, black } = theme;
+	const [noPreviousChats, setNoPreviousChats] = useState(false);
+	const [errorMessage, setErrorMessage] = useState("");
 
-  // Use useQuery to fetch the list of chats
-  const { data: chatsData, error: chatsError } = api.chat.getAll.useQuery();
+	// Use useQuery to fetch the list of chats
+	const { data: chatsData, error: chatsError } = api.chat.getAll.useQuery();
 
-  useEffect(() => {
-    if (chatsError) {
-      console.error('Error fetching chats:', chatsError);
-      setNoPreviousChats(true);
-      setErrorMessage('Error fetching data from server.');
-    }
-  }, [chatsError, chatsData]);
+	useEffect(() => {
+		if (chatsError) {
+			console.error("Error fetching chats:", chatsError);
+			setNoPreviousChats(true);
+			setErrorMessage("Error fetching data from server.");
+		}
+	}, [chatsError, chatsData]);
 
-  // If there are no previous chats, display a message
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setNoPreviousChats(true);
-    }, 3000);
+	// If there are no previous chats, display a message
+	useEffect(() => {
+		const timer = setTimeout(() => {
+			setNoPreviousChats(true);
+		}, 3000);
 
-    return () => clearTimeout(timer);
-  }, [chatsData]);
+		return () => clearTimeout(timer);
+	}, [chatsData]);
 
-  return (
-    <Flex
-      justify='flex-start'
-      align='center'
-      direction='column'
-      sx={{
-        margin: '0',
-        position: 'fixed',
-        width: '250px',
-        height: '100%',
-        zIndex: 20,
-        top: '0',
-        left: '0',
-        backgroundColor: colors?.darkPink?.[9],
-      }}
-    >
-      <Box h={100}>
-        <Button
-          onClick={onStartNewChat}
-          mt='xl'
-          mb='sm'
-          sx={{ backgroundColor: colors?.darkPink?.[6] }}
-        >
-          Start New Chat
-        </Button>
-      </Box>
+	const handleGeneratePDF = (clientId: string) => {
+		console.log("Generating PDF...");
+	};
 
-      <Box ml='md' mr='md' w='90%'>
-        <Title order={3} c={white}>
-          Previous Chats
-        </Title>
-        <Divider my='sm' color={white} />
-      </Box>
-      <ScrollArea h='95vh' ml='md' mr='md' p={0}>
-        {errorMessage && (
-          <Flex
-            justify='flex-start'
-            align='center'
-            direction='column'
-            mt='sm'
-            p='xs'
-            sx={{
-              backgroundColor: '#E5E5E5',
-              borderRadius: '10px',
-            }}
-          >
-            <Text fz={14} fw={500} c={black}>
-              {errorMessage}
-            </Text>
-          </Flex>
-        )}
+	return (
+		<Flex
+			justify='flex-start'
+			align='center'
+			direction='column'
+			sx={{
+				margin: "0",
+				position: "fixed",
+				width: "250px",
+				height: "100%",
+				zIndex: 20,
+				top: "0",
+				left: "0",
+				backgroundColor: colors?.darkPink?.[9],
+			}}
+		>
+			<Box h={100}>
+				<Button
+					onClick={onStartNewChat}
+					mt='xl'
+					mb='sm'
+					sx={{ backgroundColor: colors?.darkPink?.[6] }}
+				>
+					Start New Chat
+				</Button>
+			</Box>
 
-        {chatsData &&
-          chatsData.map((chat: any) => (
-            <Flex
-              key={chat.id}
-              align='center'
-              justify='space-between'
-              ml='md'
-              mr='md'
-              mb='sm'
-              pl={8}
-              pr={2}
-              sx={{
-                backgroundColor: 'rgba(229,229,229,0.9)',
-                borderRadius: '10px',
-                width: '225px',
-              }}
-            >
-              <Text truncate='end' fz={14} fw={700} c={black} tt={'capitalize'}>
-                {chat.title}
-              </Text>
-              <Menu position='bottom' shadow='lg' withArrow>
-                <Menu.Target>
-                  <Button
-                    p={0}
-                    m={0}
-                    w='fit-content'
-                    sx={{
-                      color: colors?.darkPink?.[6],
-                      backgroundColor: 'transparent',
-                      '&:hover': {
-                        color: colors?.pink?.[3],
-                        backgroundColor: 'transparent',
-                      },
-                    }}
-                  >
-                    <IconDotsVertical
-                      style={{ width: '20px', height: '20px' }}
-                    />
-                  </Button>
-                </Menu.Target>
-                <Menu.Dropdown sx={{ width: '200px' }}>
-                  <Menu.Label>
-                    <Text truncate='end' fw={700} tt={'capitalize'}>
-                      {chat.description}
-                    </Text>
-                  </Menu.Label>
-                  <Menu.Divider />
-                  <Menu.Item
-                    icon={
-                      <IconMessage style={{ width: '16px', height: '16px' }} />
-                    }
-                    onClick={() => onRevisitChat(chat.id)}
-                  >
-                    Revisit Chat
-                  </Menu.Item>
-                  <Menu.Item
-                    icon={
-                      <IconFileDownload
-                        style={{ width: '16px', height: '16px' }}
-                      />
-                    }
-                  >
-                    Save Chat to PDF
-                  </Menu.Item>
-                  <Menu.Item
-                    icon={
-                      <IconTrash style={{ width: '16px', height: '16px' }} />
-                    }
-                    onClick={() => onDeleteChat(chat.id)}
-                  >
-                    Delete Chat
-                  </Menu.Item>
-                </Menu.Dropdown>
-              </Menu>
-            </Flex>
-          ))}
+			<Box ml='md' mr='md' w='90%'>
+				<Title order={3} c={white}>
+					Previous Chats
+				</Title>
+				<Divider my='sm' color={white} />
+			</Box>
+			<ScrollArea h='95vh' ml='md' mr='md' p={0}>
+				{errorMessage && (
+					<Flex
+						justify='flex-start'
+						align='center'
+						direction='column'
+						mt='sm'
+						p='xs'
+						sx={{
+							backgroundColor: "#E5E5E5",
+							borderRadius: "10px",
+						}}
+					>
+						<Text fz={14} fw={500} c={black}>
+							{errorMessage}
+						</Text>
+					</Flex>
+				)}
 
-        {!chatsData &&
-          (noPreviousChats ? (
-            <Flex
-              justify='flex-start'
-              align='center'
-              direction='column'
-              mt='sm'
-              p='xs'
-              sx={{
-                backgroundColor: '#E5E5E5',
-                borderRadius: '10px',
-              }}
-            >
-              <Text fz={14} fw={700} c={black}>
-                No chat history found.
-              </Text>
-            </Flex>
-          ) : (
-            <Flex
-              justify='center'
-              align='center'
-              mt='sm'
-              sx={{ height: '100%' }}
-            >
-              <Loader color={white} size='lg' />
-            </Flex>
-          ))}
-      </ScrollArea>
-    </Flex>
-  );
+				{chatsData &&
+					chatsData.map((chat: any) => (
+						<Flex
+							key={chat.id}
+							align='center'
+							justify='space-between'
+							ml='md'
+							mr='md'
+							mb='sm'
+							pl={8}
+							pr={2}
+							sx={{
+								backgroundColor: "rgba(229,229,229,0.9)",
+								borderRadius: "10px",
+								width: "225px",
+							}}
+						>
+							<Text truncate='end' fz={14} fw={700} c={black} tt={"capitalize"}>
+								{chat.title}
+							</Text>
+							<Menu position='bottom' shadow='lg' withArrow>
+								<Menu.Target>
+									<Button
+										p={0}
+										m={0}
+										w='fit-content'
+										sx={{
+											color: colors?.darkPink?.[6],
+											backgroundColor: "transparent",
+											"&:hover": {
+												color: colors?.pink?.[3],
+												backgroundColor: "transparent",
+											},
+										}}
+									>
+										<IconDotsVertical
+											style={{ width: "20px", height: "20px" }}
+										/>
+									</Button>
+								</Menu.Target>
+								<Menu.Dropdown sx={{ width: "200px" }}>
+									<Menu.Label>
+										<Text truncate='end' fw={700} tt={"capitalize"}>
+											{chat.description}
+										</Text>
+									</Menu.Label>
+									<Menu.Divider />
+									<Menu.Item
+										icon={
+											<IconMessage style={{ width: "16px", height: "16px" }} />
+										}
+										onClick={() => onRevisitChat(chat.id)}
+									>
+										Revisit Chat
+									</Menu.Item>
+									<Menu.Item
+										icon={
+											<IconFileDownload
+												style={{ width: "16px", height: "16px" }}
+											/>
+										}
+										onClick={() => onGeneratePDF(chat.id)}
+									>
+										Save Chat to PDF
+									</Menu.Item>
+									<Menu.Item
+										icon={
+											<IconTrash style={{ width: "16px", height: "16px" }} />
+										}
+										onClick={() => onDeleteChat(chat.id)}
+									>
+										Delete Chat
+									</Menu.Item>
+								</Menu.Dropdown>
+							</Menu>
+						</Flex>
+					))}
+
+				{!chatsData &&
+					(noPreviousChats ? (
+						<Flex
+							justify='flex-start'
+							align='center'
+							direction='column'
+							mt='sm'
+							p='xs'
+							sx={{
+								backgroundColor: "#E5E5E5",
+								borderRadius: "10px",
+							}}
+						>
+							<Text fz={14} fw={700} c={black}>
+								No chat history found.
+							</Text>
+						</Flex>
+					) : (
+						<Flex
+							justify='center'
+							align='center'
+							mt='sm'
+							sx={{ height: "100%" }}
+						>
+							<Loader color={white} size='lg' />
+						</Flex>
+					))}
+			</ScrollArea>
+		</Flex>
+	);
 };
